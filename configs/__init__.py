6f137564af96e76c086570987b93f10cc7b18c0f
Andrew Kamau <andkamau@gmail.com>
2016-07-14 14:10:08 +0300

    tests for rss scraper

---
+++ configs/__init__.py
@@ -0,0 +1,5 @@
+SCRAPERS = dict(
+        rss={
+            "allafrica.com.africa": "http://allafrica.com/tools/headlines/rdf/africa/headlines.rdf"
+            }
+        )
---
+++ tests/test_scraper_rss.py
@@ -0,0 +1,39 @@
+"""
+scrapengine/scrapers/rss.py tests
+"""
+import unittest
+import requests
+import feedparser
+
+class RssScraperTestCase(unittest.TestCase):
+    
+    def setUp(self,):
+        from Scrapengine.configs import SCRAPERS
+        self.rss_source = SCRAPERS['rss']['allafrica.com.africa']
+
+    def test_get_source(self,):
+        httpresp = requests.get(self.rss_source)
+        self.assertTrue(httpresp.status_code == 200, msg="Cannot get %s. HTTP error %s" % (
+            self.rss_source, str(httpresp.status_code)))
+        del httpresp
+
+        parsed = feedparser.parse(self.rss_source)
+        self.assertIsInstance(parsed, feedparser.FeedParserDict, msg="Unexpected object type %s for %s" %
+                (type(parsed), parsed))
+        self.parsed_response = parsed
+
+    def test_response_attributes(self,):
+        self.test_get_source()
+        self.assertIn('feed', self.parsed_response)
+        self.assertIn('entries', self.parsed_response)
+        feed_metadata = self.parsed_response['feed']
+        feed_entries = self.parsed_response['entries']
+        for entry in feed_entries:
+            self.assertIn('title', entry)
+            self.assertIn('summary', entry)
+            self.assertIn('link', entry)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
