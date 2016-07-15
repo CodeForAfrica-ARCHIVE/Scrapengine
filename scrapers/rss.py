558d4e7a7408c598c6c33ca9b1bdcd8fc68cd51b
Andrew Kamau <andkamau@gmail.com>
2016-07-15 15:09:53 +0300

    proper delimiters on output

--- scrapers/rss.py
+++ scrapers/rss.py
@@ -27,10 +27,19 @@ def get_entries(parsed_response):
 def output(entries, destination='csv', source=''):
     outputfile = "%s/%s-rss-output-%s.csv" % (ARCHIVE, source, time.time())
     with open(outputfile, 'wa') as csvfile:
-        outputwriter = csv.writer(csvfile, delimiter=',')
+        outputwriter = csv.writer(csvfile, delimiter='#')
         for entry in entries:
-            outputwriter.writerow(["{id}|{published}|{title}|{summary}|{link}".format(**entry)])
-            print "writiten %s" % entry.keys()
+            outputwriter.writerow([
+                _encode(entry['id']),
+                _encode(entry['published']),
+                _encode(entry['title']),
+                _encode(entry['summary']),
+                _encode(entry['link'])
+                ])
+            print "writiten %s" % entry.get('id')
     csvfile.close()
     return outputfile
         
+
+def _encode(_unicode):
+    return _unicode.encode('utf-8')
